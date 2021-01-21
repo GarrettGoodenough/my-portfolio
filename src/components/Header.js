9ac6db4318deb3e863e1e6cbeaff02eaee75052c
GarrettGoodenough <69942588+GarrettGoodenough@users.noreply.github.com>
2021-01-20 21:26:30 -0800

    Header Complete

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import Typed from 'react-typed';
+import '../App.css'
+
+const Header = () => {
+  return (
+      <div className="header-wrapper">
+          <div className="main-info">
+              <h1>web development and website promotions</h1>
+              <Typed className="typed-text" strings={["Web Design", "Web Development", "React Programmer", "Front End Specialist"]} typeSpeed={40} backSpeed={60} loop />
+              <a href="/" className="btn-main-offer">Contact Me</a>
+          </div>
+      </div>
+  );
+};
+
+export default Header;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,55 @@
 import "../App.css";
 // REACT FONTAWESOME IMPORT
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-        <div className="container">
-            <a className="navbar-brand" href="/">
-                Goodenough Code
-            </a>
-            <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-              <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />  
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">
-                        Home <span className="sr-only">(current)</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About Me</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Services</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Portfolio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          Goodenough Code
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">
+                About Me
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">
+                Services
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">
+                Portfolio
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
