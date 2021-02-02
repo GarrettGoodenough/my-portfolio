7971ec0875b16616f2ffd5e95e3c5c0d554d5873
GarrettGoodenough <69942588+GarrettGoodenough@users.noreply.github.com>
2021-02-01 17:01:25 -0800

    Deploy Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,65 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-scroll";
 
 const Footer = () => {
-    return (
-        <div className="footer">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-4 col-md-6 col-sm-6">
-                        <div className="d-flex">
-                            <p>Address</p>
-                        </div>
-                        <div className="d-flex">
-                            <a href="tel:8184009601">818-400-9601</a>
-                        </div>
-                        <div className="d-flex">
-                            <p>garrettgoodenough18@yahoo.com</p>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-2 col-sm-6">
-                        <div className="row">
-                            <div className="col">
-                                <a className="footer-nav">Home</a>
-                                <br/>
-                                <a className="footer-nav">About Me</a>
-                                <br/>
-                                <a className="footer-nav">Portfolio</a>
-                                <br/>
-                                <a className="footer-nav">Contact</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 col-md-6 col-sm-6">
+            <div className="d-flex">
+              <p>Address</p>
             </div>
+            <div className="d-flex">
+              <a href="tel:8184009601">818-400-9601</a>
+            </div>
+            <div className="d-flex">
+              <p>garrettgoodenough18@yahoo.com</p>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-2 col-sm-6">
+            <div className="row">
+              <div className="col">
+                <Link smooth={true} to="home" className="footer-nav" href="#">
+                  Home
+                </Link>
+                <br />
+                <Link
+                  smooth={true}
+                  to="about"
+                  offset={-87}
+                  className="footer-nav"
+                  href="#"
+                >
+                  About
+                </Link>
+                <br />
+                <Link
+                  smooth={true}
+                  to="portfolio"
+                  offset={-87}
+                  className="footer-nav"
+                  href="#"
+                >
+                  Portfolio
+                </Link>
+                <br />
+                <Link
+                  smooth={true}
+                  to="contact"
+                  offset={-87}
+                  className="footer-nav"
+                  href="#"
+                >
+                  Contact
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,6 @@
 import kohSamui from "../images/koh-samui.png";
 import wildFireTracker from "../images/wildfire-tracker.png";
+import realEstate from "../images/real-estate.png";
 // FONT AWESOME IMPORT
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
@@ -8,7 +9,7 @@ import { PopupboxManager, PopupboxContainer } from "react-popupbox";
 import "react-popupbox/dist/react-popupbox.css";
 
 const Portfolio = () => {
-  // KOH SAMUI
+  /******************************** Koh Samui *******************************************/
   const openPopupboxKohSamui = () => {
     const content = (
       <>
@@ -17,18 +18,25 @@ const Portfolio = () => {
           src={kohSamui}
           alt="Koh Samui Website Project"
         />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
-          esse.
+        <p className="protfolio-text">
+          Koh Samui, Thailand website showcasing HTML, CSS, and SASS.
         </p>
+        <b>Demo: </b>
+        <a
+          className="hyper-link"
+          href="https://koh-samui.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://koh-samui.netlify.app/
+        </a>
+        <br />
         <b>GitHub:</b>{" "}
         <a
           className="hyper-link"
-          onClick={() =>
-            window.open(
-              "https://github.com/GarrettGoodenough/Koh-Samui-Website"
-            )
-          }
+          href="https://github.com/GarrettGoodenough/Koh-Samui-Website"
+          target="_blank"
+          rel="noreferrer"
         >
           https://github.com/GarrettGoodenough/Koh-Samui-Website
         </a>
@@ -46,7 +54,53 @@ const Portfolio = () => {
     fadeInSpeed: 500,
   };
 
-  // Wildfire Tracker
+  /******************************** Real-Estate *******************************************/
+  const openPopupboxRealEstate = () => {
+    const content = (
+      <>
+        <img
+          className="portfolio-image-popupbox"
+          src={realEstate}
+          alt="Real-Estate Project"
+        />
+        <p>
+          Real Estate project showcaseing my understanding of React, React
+          Router, and various animations.
+        </p>
+        <b>Demo: </b>
+        <a
+          className="hyper-link"
+          href="https://guarded-chamber-32196.herokuapp.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://guarded-chamber-32196.herokuapp.com/
+        </a>
+        <br />
+        <b>GitHub: </b>
+        <a
+          className="hyper-link"
+          href="https://github.com/GarrettGoodenough/goodenough-real-estate"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://github.com/GarrettGoodenough/goodenough-real-estate
+        </a>
+      </>
+    );
+    PopupboxManager.open({ content });
+  };
+
+  const popupboxConfigRealEstate = {
+    titleBar: {
+      enable: true,
+      text: "Real-Estate Project",
+    },
+    fadeIn: true,
+    fadeInSpeed: 500,
+  };
+
+  /******************************** Wildfire Tracker *******************************************/
   const openPopupboxWildfire = () => {
     const content = (
       <>
@@ -56,17 +110,25 @@ const Portfolio = () => {
           alt="Wildfire Tracker Project"
         />
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
-          esse.
+          Application demonstarting my knowledge of using React to make API
+          requests.
         </p>
-        <b>GitHub:</b>{" "}
+        <b>Demo: </b>
         <a
           className="hyper-link"
-          onClick={() =>
-            window.open(
-              "https://github.com/GarrettGoodenough/wildfire-tracker-project"
-            )
-          }
+          href="https://serene-stream-07774.herokuapp.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://serene-stream-07774.herokuapp.com/
+        </a>
+        <br />
+        <b>GitHub: </b>
+        <a
+          className="hyper-link"
+          href="https://github.com/GarrettGoodenough/wildfire-tracker-project"
+          target="_blank"
+          rel="noreferrer"
         >
           https://github.com/GarrettGoodenough/wildfire-tracker-project
         </a>
@@ -97,6 +159,17 @@ const Portfolio = () => {
             />
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+            <p className="portfolio-description">CSS & SASS</p>
+          </div>
+          <div className="portfolio-image-box" onClick={openPopupboxRealEstate}>
+            <img
+              className="portfolio-img"
+              src={realEstate}
+              alt="Real Estate Website"
+            />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+            <p className="portfolio-description">React & React Router</p>
           </div>
           <div className="portfolio-image-box" onClick={openPopupboxWildfire}>
             <img
@@ -106,10 +179,12 @@ const Portfolio = () => {
             />
             <div className="overflow"></div>
             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+            <p className="portfolio-description">API Fetch Requests</p>
           </div>
         </div>
       </div>
       <PopupboxContainer {...popupboxConfigKohSamui} />
+      <PopupboxContainer {...popupboxConfigRealEstate} />
       <PopupboxContainer {...popupboxConfigWildfireTracker} />
     </div>
   );
